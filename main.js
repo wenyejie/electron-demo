63bfbf7ac030c43ac496b8afe46af1ef7286984b
wenyejie <wenyejie@foxmail.com>
2023-07-07 17:54:46 +0800

    使用iohook全局监听Ctrl+Sheift+Wheel Successful!

--- main.js
+++ main.js
@@ -1,41 +1,61 @@
-const { app, BrowserWindow, ipcMain, globalShortcut } = require('electron')
-const path = require('path')
+// 主进程
+const {
+  app,
+  BrowserWindow,
+  screen,
+  ipcMain,
+  globalShortcut,
+  ipcRenderer,
+} = require('electron')
 
-function createWindow () {
-  const win = new BrowserWindow({
+const iohook = require('iohook')
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
-  globalShortcut.register('CommandOrControl+E', () => {
-    console.log('你按下了Ctrl+E')
-  })
-
-  win.webContents.on('zoom-changed', (event, zoomDirection) => {
-    console.log(event, zoomDirection)
+      nodeIntegration: true,
+    },
   })
 
   win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  ipcMain.handle('ping', () => {
-    return 'pong'
-  })
   createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+  console.log('iohook')
+
+  let wheelIsDown = false
+
+  iohook.on('mousewheel', (event) => {
+    // console.log('mousewheel', event)
+  })
+  iohook.on('mousedown', (event) => {
+    if (event.button === 3) {
+      wheelIsDown = true
+      // console.log('mousedown', event)
     }
   })
-})
+  iohook.on('mouseup', (event) => {
+    if (event.button === 3) {
+      wheelIsDown = false
+      // console.log('mouseup', event)
+    }
+  })
+  /* iohook.on('keydown', (event) => {
+    console.log('keydown', event)
+  }) */
+  iohook.registerShortcut([29, 42], (keys) => {
+    // console.log('Ctrl+Shift', keys)
+    if (wheelIsDown) {
+      console.log('you use Ctrl+Shift+Wheel!!!' + Math.random(1))
+    }
+  })
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  iohook.start(false)
 })
