a91b10f76920f2ff579d37b4e32853598d11d64b
wenyejie <wenyejie@foxmail.com>
2023-07-28 18:30:28 +0800

    调整屏幕授权功能测试

--- desktopCapturer/main.js
+++ desktopCapturer/main.js
@@ -1,4 +1,4 @@
-const { app, screen, ipcMain, BrowserWindow, desktopCapturer, systemPreferences } = require('electron')
+const { app, screen, BrowserWindow, desktopCapturer, systemPreferences } = require('electron')
 const path = require('path')
 
 let win
@@ -15,51 +15,38 @@ const createWindow = () => {
   win.loadFile('./desktopCapturer/index.html')
 }
 
-const screenCapture = () => {
-  return new Promise((resolve, reject) => {
-    const { size: { width, height }, scaleFactor } = screen.getPrimaryDisplay()
-    desktopCapturer.getSources({
-      types: ['screen'],
-      thumbnailSize: {
-        width: Math.floor(width * scaleFactor),
-        height: Math.floor(height * scaleFactor)
-      }
-    }).then(response => {
-      const thumbnails = []
-      response.forEach(({ thumbnail }) => {
-        thumbnails.push(thumbnail.toDataURL())
-      })
-      resolve(thumbnails)
-    }).catch(e => {
-      reject(e)
-    })
-  })
-}
-
 const hasScreenCaptureAccess = () => {
   return systemPreferences.getMediaAccessStatus('screen') === 'granted'
+}
+
+const tryScreenCapture = () => {
+  return desktopCapturer.getSources({
+    types: ['screen'],
+    thumbnailSize: {
+      width: 100,
+      height: 100,
+    }
+  }).then(sources => {
+    console.log('screen capture success', sources.length > 0)
+    return Promise.resolve(sources.length > 0)
+  }, reason => {
+    console.log('screen capture failed', reason)
+    return Promise.reject(reason)
+  }).catch(error => {
+    console.log('screen capture failed', error)
+    return Promise.reject(error)
+  })
 }
 
 const askScreenCaptureAccess = () => {
   if (hasScreenCaptureAccess()) {
     return true
   }
-  return systemPreferences.askForMediaAccess('screen')
-}
 
-async function requestScreenCapturePermission() {
-  const status = systemPreferences.getMediaAccessStatus('screen')
-  if (status === 'not-determined') {
-    const success = await systemPreferences.askForMediaAccess('screen')
-    if (success) {
-      console.log('用户已授权屏幕录制权限')
-    } else {
-      console.log('用户拒绝了屏幕录制权限')
-    }
-  } else if (status === 'granted') {
-    console.log('应用程序已经具有屏幕录制权限')
-  } else {
-    console.log('应用程序没有屏幕录制权限')
+  try {
+    return systemPreferences.askForMediaAccess('screen')
+  } catch (e) {
+    return tryScreenCapture()
   }
 }
 
